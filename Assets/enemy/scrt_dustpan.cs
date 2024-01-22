134ef01f91457f25d1d2e383998c96e623c9f3c0
bjr7000 <>
2024-01-22 17:38:53 +0900

    [test] push test

--- Assets/enemy/scrt_dustpan.cs
+++ Assets/enemy/scrt_dustpan.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class scrt_dustpan : MonoBehaviour
 {
+    int attack = 20;
+    float health = 100;
+    int delay = 30;
+    int speed = 10;
+    int state = 0; //0: normal, 1: alert, 2: stunned, 3: dead
+    int direction = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,22 @@ public class scrt_dustpan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        delay--;
+        if (health <= 0) { state = 3; }
+    }
+
+    void Move()
+    {
+        if (state != 3) { transform.Translate(Vector3.right*direction*speed * Time.deltaTime, Space.World); }
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Damage()
+    {
+
     }
 }
