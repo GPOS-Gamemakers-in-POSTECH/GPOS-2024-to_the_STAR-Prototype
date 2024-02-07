69b7423168f7a37eef452d3c37bd789e4feb4672
bjr7000 <>
2024-02-07 18:20:47 +0900

    [Chore] system assets folder and script for wall are created

---
+++ Assets/system assets/scrt_wall.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrt_wall : MonoBehaviour
+{
+
+    public int wallLoc = 0;
+    
+    void Start()
+    {
+        gameObject.tag = "wall";
+    }
+
+    void Update()
+    {
+        
+    }
+}
