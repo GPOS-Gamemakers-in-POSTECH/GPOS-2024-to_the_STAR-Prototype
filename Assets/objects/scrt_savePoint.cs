48f8b8cf8b75ecdbbdd7129f944de5a36af25c21
bjr7000 <>
2024-02-14 15:02:07 +0900

    [Feat] save point & player's revival

--- Assets/objects/scrt_savePoint.cs
+++ Assets/objects/scrt_savePoint.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class scrt_savePoint : MonoBehaviour
 {
+    GameObject player;
 
     void Start()
     {
         gameObject.tag = "object";
+        player = GameObject.FindWithTag("player");
     }
 
     void Update()
@@ -17,6 +19,7 @@ public class scrt_savePoint : MonoBehaviour
 
     public void interAct() //상호작용을 할 때 해당 변수를 호출하면 됨
     {
-
+        player.GetComponent<scrt_player>().reviveX = transform.position.x;
+        player.GetComponent<scrt_player>().reviveY = transform.position.y;
     }
 }
--- Assets/player/scrt_player.cs
+++ Assets/player/scrt_player.cs
@@ -7,6 +7,9 @@ public class scrt_player : MonoBehaviour
 {
     float health = 100f;
 
+    public float reviveX;
+    public float reviveY;
+
     Animator animator;
 
     // Start is called before the first frame update
@@ -14,6 +17,8 @@ public class scrt_player : MonoBehaviour
     {
         gameObject.tag = "player";
         animator = GetComponent<Animator>();
+        reviveX = transform.position.x;
+        reviveY = transform.position.y;
     }
 
     // Update is called once per frame
@@ -32,5 +37,11 @@ public class scrt_player : MonoBehaviour
     public void GetStunned(float time) //스턴당하는 함수
     {
         animator.SetTrigger("trigger_getStunned");
+    }
+
+    public void Revive()
+    {
+        animator.SetBool("bool_death", false);
+        transform.position = new Vector3(reviveX, reviveY, 0f);
     }
 }
