a4b296b5ec7d8bbf0f8123186e8afd2179680cc4
a-nodi <leeyoonhyuk0@naver.com>
2024-02-15 17:02:51 +0900

    [Feat] Updated Gravity update

--- Assets/player/GravityDirectionControl.cs
+++ Assets/player/GravityDirectionControl.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GravityDirectionControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
 
+    }
+
+    void FixedUpdate()
+    {
+        UpdateGravityVector();    
     }
 
     public void UpdateGravityVector()
