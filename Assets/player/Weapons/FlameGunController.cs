6b4fa87b2cdfa804e06c6ef86e53108900b9e33b
Mingyu Nam <mgnam@postech.ac.kr>
2024-02-18 21:52:36 +0900

    [Fix] Fix FireGun sprite rotation

--- Assets/player/Weapons/FlameGunController.cs
+++ Assets/player/Weapons/FlameGunController.cs
@@ -8,10 +8,15 @@ public class FlameGunController : MonoBehaviour
     private float angle;
     private Vector2 target, mouse;
 
+    // sprite
+    private SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.tag = "playerWeapon";
+
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,5 +27,14 @@ public class FlameGunController : MonoBehaviour
         mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
         this.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (angle > 90 || angle < -90)
+        {
+            sr.flipY = true;
+        }
+        else
+        {
+            sr.flipY = false;
+        }
     }
 }
